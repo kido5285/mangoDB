612bcc838b92f67b877199fa045d1d3576ccbef8
kido5285 <75468540+kido5285@users.noreply.github.com>
2021-02-07 13:00:31 +0800

    Add files via upload

---
+++ Video_Flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import Blueprint, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+app = Flask(__name__)
+app.debug = True
+main = Blueprint('main', __name__)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    comment_text = db.Column(db.String(1000))
+
+@main.route('/')
+def index():
+    app.config['SQLAlchemy＿DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    db.init_app(app)
+    return render_template('index.html')
+
+@main.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@main.route('/sign', methods=['POST'])
+def sign_post():
+    name = request.form.get('name')
+    comment = request.form.get('comment')
+    return f'Name: {name}, Comment: {comment}'
+
+app.register_blueprint(main)
+if __name__=='__main__':
+    app.run(debug=True)
